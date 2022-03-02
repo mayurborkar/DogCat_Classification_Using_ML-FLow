d9877081740f0ed8f72a61a26e6d311e02a116d9
Mayur Borkar <borkarm112@gmail.com>
2022-03-02 15:21:11 +0530

    Validation Add In Stage 1

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -1,56 +1,50 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
 from src.utils.common import read_yaml, create_directories, unzip_files
 from src.utils.data_mgmt import validate_image
-import urllib.request as req
-from tqdm import tqdm
-import argparse
-import logging
 import random
-import shutil
-import os
+import urllib.request as req
 
 STAGE = "GET_DATA" ## <<< change stage name 
 
 logging.basicConfig(
-                    filename=os.path.join("logs", 'running_logs.log'), 
-                    level=logging.INFO, 
-                    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
-                    filemode="a"
-                    )
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
 
 
 def main(config_path):
-
     ## read config files
     config = read_yaml(config_path)
     URL = config["data"]["source_url"]
-
     local_dir = config["data"]["local_dir"]
     create_directories([local_dir])
 
     data_file = config["data"]["data_file"]
     data_file_path = os.path.join(local_dir, data_file)
 
-    #Check The File Is Present Or Not. If Not Download tHe Zip File
+    
     if not os.path.isfile(data_file_path):
-        logging.info(f"Downloading Started....")
-
+        logging.info("downloading started...")
         filename, headers = req.urlretrieve(URL, data_file_path)
         logging.info(f"filename:{filename} created with info \n{headers}")
-
     else:
-        logging.info(f"Filename {data_file} Already present")
+        logging.info(f"filename:{data_file} already present")
 
-    #Unzip The Data
-    unzip_data_dir= config["data"]["unzip_data_dir"]
+    # Unzip ops
+    unzip_data_dir = config["data"]["unzip_data_dir"]
     if not os.path.exists(unzip_data_dir):
         create_directories([unzip_data_dir])
         unzip_files(source=data_file_path, dest=unzip_data_dir)
     else:
-        logging.info(f"Data Already Extracted")
-
+        logging.info(f"data already extracted")
     # validating data
     validate_image(config)
-
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
