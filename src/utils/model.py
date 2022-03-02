f75858358c97f2bdf3c6d1c03590a4ded495ca3b
Mayur Borkar <borkarm112@gmail.com>
2022-03-02 14:24:56 +0530

    Stage 2 Added

---
+++ src/utils/model.py
@@ -0,0 +1,10 @@
+import logging
+import io
+
+def log_model_summary(model):
+    with io.StringIO() as stream:
+        model.summary(
+            print_fn = lambda x: stream.write(f'{x}\n')
+        )
+        summary_str = stream.getvalue()
+    return summary_str
