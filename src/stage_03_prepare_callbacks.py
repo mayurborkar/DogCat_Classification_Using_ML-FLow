d673d07a18d545b360db6e8bcb63e97bb31567e2
Mayur Borkar <borkarm112@gmail.com>
2022-03-02 17:09:59 +0530

    Stage 3 & 4 add

---
+++ src/stage_03_prepare_callbacks.py
@@ -0,0 +1,37 @@
+from src.utils.common import read_yaml, create_directories
+from tqdm import tqdm
+import argparse
+import logging
+import random
+import shutil
+import os
+
+STAGE = "STAGE_NAME" ## <<< change stage name 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def main(config_path):
+    ## read config files
+    config = read_yaml(config_path)
+    pass
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
